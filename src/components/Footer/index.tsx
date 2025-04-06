c9836abe15ed50f295f588268e6edb9315ef685f
Alex <alffreitas@outlook.com>
2025-04-05 21:54:46 -0300

    refactor: next 15

--- src/components/Footer/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="bg-gray-800 text-white flex items-center justify-center p-5">
-            Copyright {new Date().getFullYear()}
-        </footer>
-    )
-}
